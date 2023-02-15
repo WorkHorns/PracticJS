979f0a6a95a4a595bc96a364fcc714695a647b62
SergeevA <workhorns@gmail.com>
2023-02-15 09:03:54 +0700

    Выполнено 2 задание

--- script.js
+++ script.js
@@ -17,10 +17,24 @@ for(i=0; i<2; i++)
     
     if(a != null && b != null && a != ' ' && b != ' ' && a.length <= 50)
     {
+        console.log("ok")
         personalMovie.movies[a] = b;
     }
     else 
+        console.log("error")
         i--;
 }
 
+if(personalMovie.count < 10)
+{
+    console.log("Мало.")
+}
+else if(personalMovie.count >= 10 && personalMovie.count < 30)
+{
+    console.log("Киноман.")
+}
+else (personalMovie.count >= 30)
+{
+    console.log("Ошибка")
+}
 console.log(personalMovie);
