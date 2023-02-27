c51c454caf2aa3777da3652147bb6fa9fd2c771e
SergeevA <workhorns@gmail.com>
2023-02-27 10:13:36 +0700

    Работа с датами

--- preview/script.js
+++ preview/script.js
@@ -3,3 +3,35 @@
 // Работа с обработчиками.
 // Работа с класом owerlay.
 // Работа с ссылками. Сокращения.
+
+// const dateNow = new Date('2023-02-28');
+// new dateNow.parset('2023-02-28');
+
+// console.log(dateNow.getFullYear());
+// console.log(dateNow.getMonth() + 1);
+// console.log(dateNow.getDate());
+// console.log(dateNow.getDay());
+// console.log(dateNow.getUTCHours());
+// console.log(dateNow.getHours());
+
+// console.log(dateNow.getTimezoneOffset());
+// console.log(dateNow.getTime());
+
+// console.log(dateNow.setHours(40));
+// console.log(dateNow);
+
+// let start = new Date();
+
+// for(i=0; i < 1000000; i++)
+// {
+//     let some = i ** 3;
+// }
+
+// let end = new Date();
+
+// alert(`${end - start}`)
+
+
+
+
+
