6166bfcc512d91ab40f69cc037da8435db278e68
SergeevA <workhorns@gmail.com>
2023-03-01 13:49:05 +0700

    Работа с елементами страницы

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,2 +1,25 @@
 'use strict';
 // По играть со стилями, поиграть с формами
+const a = document.getElementById('box');
+console.log(a);
+
+const b = document.getElementsByTagName('button');
+console.log(b[4]);
+
+const c = document.getElementsByClassName('circle');
+console.log(c);
+
+const q = document.querySelectorAll('.heart');
+console.log(q);
+
+q.forEach(item => {
+    console.log(item);
+});
+
+const qq = document.querySelector('.heart');
+console.log(qq);
+
+const qw = document.querySelector('div');
+console.log(qw);
+
+    
