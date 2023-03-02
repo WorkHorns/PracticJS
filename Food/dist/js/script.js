76b318dfc95ff87c1664e9e35c1be46e4404f134
SergeevA <workhorns@gmail.com>
2023-03-02 14:08:49 +0700

    1

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -246,4 +246,57 @@ window.addEventListener('DOMContentLoaded',
             'menu__item',
             'big'
         ).render();
+            //Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'OK',
+        fail: 'Fail'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form){
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json');
+
+            const formData = new FormData(form);
+
+            const obj = {};
+
+            formData.forEach(function(value, key){
+                obj[key] = value;
+            });
+
+            request.send(JSON.stringify(obj));
+
+            request.addEventListener('load', ()=> {
+                if(request.status === 200) {
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() =>{
+                        statusMessage.remove();
+                    },3000);
+                }
+                else {
+                    statusMessage.textContent = message.fail;
+                    form.reset();
+                }
+            })
+        });
+    };
+
 });
