4f1b7f867b05d707a0665b61ea5f3d4c56989471
SergeevA <workhorns@gmail.com>
2023-03-06 14:25:54 +0700

    Работа с инкапсуляцией

--- Food/dist/js/qualityScript.js
+++ Food/dist/js/qualityScript.js
@@ -1,14 +1,47 @@
-const pers = {
-    name: 'alert',
-    age: 12,
+class Pers{
+    constructor(name, age) {
+        this.name = name,
+        this._age = age;
+    }
     
-    get userAge() {
-        return this.age; // Получение возраста
-    },
-    set userAge(num) {
-        this.age = num; //Присвоение возроста
+    surname = 'Sergeev';
+
+    get sure() {
+        return this._surname;
+    }
+
+    set sure(surname) {
+        this._surname = surname;
+    }
+
+
+    say = () => {
+        console.log(`Имя человека: ${this.name} ${this.surname} возрост: ${this._age}.`)
+    }
+    
+    get age() {
+        return this._age;
+    }
+    
+    set age(age) {
+        if(age > 0 && age < 100 && typeof age === 'number'){
+            this._age = age;
+        } else {
+            
+            console.log('Не корректный возраст.')
+        }
     }
 };
 
-console.log(pers.userAge);
-console.log(pers.userAge = 30);
+const al = new Pers('al', 23);
+// console.log(al.surname);
+// al.age = 88;
+// console.log(al.age);
+al.say();
+
+al.surname = 'Бобков';
+
+al.say();
+
+
+
