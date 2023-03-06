c91dfe5db07cfaa871699f19bb33e5f200e5f547
SergeevA <workhorns@gmail.com>
2023-03-06 11:42:18 +0700

    Создание скрипта калькулятора 2.0

--- Food/dist/js/calcScript.js
+++ Food/dist/js/calcScript.js
@@ -1,9 +1,19 @@
 const result = document.querySelector('.calculating__result span');
-let sex = 'female', 
-    height, 
-    weight, 
-    age, 
+let sex, height, weight, age, ratio;
+
+if(localStorage.getItem('sex')) {
+    sex = localStorage.getItem('sex');
+} else {
+    sex = 'female';
+    localStorage.setItem('sex', 'female');
+}
+
+if(localStorage.getItem('ratio')) {
+    ratio = localStorage.getItem('ratio');
+} else {
     ratio = 1.375;
+    localStorage.setItem('ratio', 1.375);
+}
 
     //Калькулятор
     function calcTotal(){
@@ -20,15 +30,17 @@ let sex = 'female',
         }
     }
     //Получение статической информации из калькулятора
-    function getStaticInformation(parentSelector , activeClass){
-        const elements = document.querySelectorAll(`${parentSelector} div`);
+    function getStaticInformation(selector , activeClass) {
+        const elements = document.querySelectorAll(selector);
 
         elements.forEach(element => {
             element.addEventListener('click', (event)=>{
                 if(event.target.getAttribute('data-ratio') ) {
                     ratio = +event.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', +event.target.getAttribute('data-ratio'));
                 } else {
                     sex = event.target.getAttribute('id');
+                    localStorage.setItem('sex', event.target.getAttribute('id'));
                 }
     
                 elements.forEach(element =>{
@@ -45,6 +57,11 @@ let sex = 'female',
         const input = document.querySelector(selector);
 
         input.addEventListener('input', () => {
+            if(input.value.match(/\D/g)){
+                input.style.border = '1px solid red';
+            } else {
+                input.style.border = 'none';
+            }
             switch(input.getAttribute('id')){
                 case 'height':
                         height = +input.value;
@@ -59,10 +76,26 @@ let sex = 'female',
             calcTotal();
         });
     };
+    //
+    function getLocalStoredge(selector, activeClass) {
+        const element = document.querySelectorAll(selector);
+        element.forEach(item => {
+            item.classList.remove(activeClass);
+            if(item.getAttribute('id') === localStorage.getItem('sex')) {
+                item.classList.add(activeClass);
+            }
+            if(item.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                item.classList.add(activeClass);
+            }
+        });
+    };
+
 //Вызовы
+getLocalStoredge('#gender div','calculating__choose-item_active');
+getLocalStoredge('.calculating__choose_big div','calculating__choose-item_active');
 calcTotal();
-getStaticInformation('#gender','calculating__choose-item_active');
-getStaticInformation('.calculating__choose_big','calculating__choose-item_active');
+getStaticInformation('#gender div','calculating__choose-item_active');
+getStaticInformation('.calculating__choose_big div','calculating__choose-item_active');
 getDinamicInformation('#height');
 getDinamicInformation('#weight');
 getDinamicInformation('#age');
